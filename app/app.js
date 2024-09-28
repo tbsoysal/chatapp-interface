9d0f32bbd75021ca9a0a060ba76f9bcec08fb206
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-09-28 14:19:54 +0300

    start building servers

---
+++ app/app.js
@@ -0,0 +1,49 @@
+const socket = io('ws://localhost:3500');
+const input = document.querySelector("#message-box");
+const form = document.querySelector("form");
+const chatEl = document.querySelector("main");
+
+function sendMessage(e) {
+  e.preventDefault();
+  if (input.value) {
+    showSendingMessage(input.value);
+    socket.emit("message", input.value);
+    input.value = '';
+    scrollToBottom();
+  }
+  input.focus();
+}
+
+form.addEventListener("submit", sendMessage);
+
+socket.on("message", data => showIncomingMessage(data))
+
+function showIncomingMessage(message) {
+  const messageContainer = document.createElement('div');
+  messageContainer.classList.add('incoming-message');
+  messageContainer.innerHTML = 
+    `
+      <p>${message}</p>
+      <span class="info">
+        <span class="time">16:56</span>
+      </span>`;
+  chatEl.append(messageContainer);
+  scrollToBottom();
+}
+
+function showSendingMessage(message) {
+  const messageContainer = document.createElement('div');
+  messageContainer.classList.add('message');
+  messageContainer.innerHTML = 
+    `
+      <p>${message}</p>
+      <span class="info">
+        <span class="time">16:56</span>
+        <img src="./Vector.svg" alt="seen">
+      </span>`;
+  chatEl.append(messageContainer);
+}
+
+function scrollToBottom() {
+  chatEl.scrollTop = chatEl.scrollHeight;
+}
---
+++ server/index.js
@@ -0,0 +1,21 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.NODE_ENV === "production" ? false :
+      ["http://localhost:8080", "http://127.0.0.1:8080"]
+  }
+})
+
+io.on('connection', socket => {
+  console.log(`User ${socket.id.substring(0, 5)} connected!`);
+  
+  socket.on('message', data => {
+    console.log(`${socket.id.substring(0, 5)}: ${data}`);
+    io.emit("message", `${data}`);
+  })
+})
+
+httpServer.listen(3500, () => console.log(`Server is listening on http://localhost:3500`));
